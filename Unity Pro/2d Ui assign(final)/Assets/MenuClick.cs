8e3c8c4df007a91e0f16b41c26d20fe1cc434deb
jewon1210 <49775206+jewon1210@users.noreply.github.com>
2020-04-28 18:52:52 +0900

    ui 과제 상호작용까지 구현한것입니다.
    ui 과제 상호작용까지 구현한것입니다.

---
+++ Unity Pro/2d Ui assign(final)/Assets/MenuClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuClick : MonoBehaviour
+{
+    [SerializeField] GameObject MenuButton;
+    bool ClickCheck = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ClickMenuButton()
+    {
+        ClickCheck = true;
+        MenuButton.SetActive(ClickCheck);
+    }
+
+    public void HomeButton()
+    {
+        ClickCheck = false;
+        MenuButton.SetActive(ClickCheck);
+    }
+}
---
+++ Unity Pro/2d Ui assign(final)/Assets/SoundClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundClick : MonoBehaviour
+{
+    [SerializeField] GameObject OnSoundButton;
+    [SerializeField] GameObject OffSoundButton;
+
+    bool SoundCheck = true;//소리있엉
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnOffSound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnOffSound()
+    {
+        if(SoundCheck==true)
+        {
+            OnSoundButton.SetActive(true);
+            OffSoundButton.SetActive(false);
+            SoundCheck = false;
+        }
+
+        else
+        {
+            OnSoundButton.SetActive(false);
+            OffSoundButton.SetActive(true);
+            SoundCheck = true;
+        }
+    }
+
+}
